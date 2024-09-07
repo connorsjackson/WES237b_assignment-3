22f088434cbb39db45bbaf3f4688ce4beec4f2af
Connors <c9jackson@ucsd.edu>
2024-09-07 07:59:29 -0700

    tiled rev-

--- homework/opencl_tiled_mm/kernel.cl
+++ homework/opencl_tiled_mm/kernel.cl
@@ -1,8 +1,34 @@
+#define tile_size 16
+
 __kernel void matrixMultiply(
     __global const float *A, __global const float *B, __global float *C,
     const unsigned int numARows, const unsigned int numAColumns,
     const unsigned int numBRows, const unsigned int numBColumns,
     const unsigned int numCRows, const unsigned int numCColumns) {
   //@@ Insert code to implement matrix multiplication here
+  __local float A_kernel[tile_size][tile_size];
+  __local float B_kernel[tile_size][tile_size];
+  int row_kernel = local(0);
+  int col_kernel = local(1);
+  int row_main = tile_size * get_group_id(0) + row_kernel(0);
+  int col_main = tile_size * group(0) + col_kernel(0);
 
+  int num_tiles = (numAColumns + tile_size) / tile_size;
+  float accum = 0;
+
+  for (int tile = 0; tile < num_tiles;){
+    tile_row = tile_size * tile + row_kernel;
+    tile_col = tile_size * tile + col_kernel;
+  }
 }
+
+/* Naive MM is below
+for (int i = 0; i< A_row ; i++){
+  for (int j=0; j < B_col; j++){
+    for (int k=0; k<A_col; k++){
+      // C[x,y] = A[x,k] + B[k,y]
+      C[i*A_row + j] += A[i*A_col + k] + B[k*B_col + j]
+    }
+  }
+}
+*/
--- homework/opencl_tiled_mm/main.c
+++ homework/opencl_tiled_mm/main.c
@@ -61,8 +61,13 @@ void OpenCLMatrixMultiply(Matrix *input0, Matrix *input1, Matrix *result)
     CHECK_ERR(err, "clCreateKernel");
 
     //@@ Allocate GPU memory here
+    device_a = clCreateBuffer(context, CL_MEM_WRITE_ONLY, (*input0).shape[0] * (*input0).shape[1] * sizeof(float), NULL, &err);
+    device_b = clCreateBuffer(context, CL_MEM_WRITE_ONLY, (*input1).shape[0] * (*input1).shape[1] * sizeof(float), NULL, &err);
+    device_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, (*result).shape[0] * (*result).shape[1] * sizeof(float), NULL, &err);
 
     //@@ Copy memory to the GPU here
+    err = clEnqueueWriteBuffer(queue, device_a, CL_TRUE, 0, (*input0).shape[0] * (*input0).shape[1] * sizeof(float), (*input0).data, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(queue, device_b, CL_TRUE, 0, (*input1).shape[0] * (*input1).shape[1] * sizeof(float), (*input1).data, 0, NULL, NULL);
 
     // Set the arguments to our compute kernel
     // __global const float *A, __global const float *B, __global float *C,
@@ -89,12 +94,40 @@ void OpenCLMatrixMultiply(Matrix *input0, Matrix *input1, Matrix *result)
     CHECK_ERR(err, "clSetKernelArg 8");
 
     // @@ define local and global work sizes
+    size_t global_item_size[2], local_item_size[2];
+    if ((*result).shape[0] %% tile_size == 0){
+        global_item_size[0] = (*result).shape[0];
+    }
+    else{
+        global_item_size[0] = (floor((*result).shape[0] / tile_size) + 1) * tile_size;
+    }
+    if ((*result).shape[1] %% tile_size == 0){
+        global_item_size[1] = (*result).shape[1];
+    }
+    else{
+        global_item_size[1] = (floor((*result).shape[1] / tile_size) + 1) * tile_size;
+    }
+    local_item_size[0] = tile_size;
+    local_item_size[1] = tile_size;
 
     //@@ Launch the GPU Kernel here
+    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_item_size, local_item_size, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueNDRangeKernel");
+    clFinish(queue);
 
     //@@ Copy the GPU memory back to the CPU here
+    err = clEnqueueReadBuffer(queue, device_c, CL_TRUE, 0, (*result).shape[0] * (*result).shape[1] * sizeof(float), (*result).data, 0, NULL, NULL);
+    CHECK_ERR(err, "clEnqueueWriteBuffer device_c");
 
     //@@ Free the GPU memory here
+    clReleaseMemObject(device_a);
+    clReleaseMemObject(device_b);
+    clReleaseMemObject(device_c);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+    free(kernel_source);
 }
 
 int main(int argc, char *argv[])
@@ -127,6 +160,8 @@ int main(int argc, char *argv[])
     int rows, cols;
     //@@ Update these values for the output rows and cols of the output
     //@@ Do not use the results from the answer matrix
+    rows = host_a.shape[0];
+    cols = host_b.shape[1];
 
     // Allocate the memory for the target.
     host_c.shape[0] = rows;
